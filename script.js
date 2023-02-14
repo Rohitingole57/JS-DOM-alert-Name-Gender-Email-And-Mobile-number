63b5021d2dca12ec8b19308dc7927ed1ddf61377
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 10:40:55 +0530

    styled and pushed

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function entername() {
+    var name3 = document.getElementById("gem").value
+    // document.getElementById("demoo").innerText = name3
+    alert(name3)
+}
+function entergender() {
+    var name4 = document.getElementById("butt").value
+    // document.getElementById("demooo").innerText = name4
+    alert(name4)
+}
+function enteremail() {
+    var name5 = document.getElementById("eid").value
+    // document.getElementById("demoooo").innerText = name5
+    alert(name5)
+}
+function enternumber() {
+    var name6 = document.getElementById("mno").value
+    // document.getElementById("demooooo").innerText = name6
+    alert(name6)
+}
